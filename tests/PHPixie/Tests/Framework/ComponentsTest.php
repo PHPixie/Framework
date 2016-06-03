ec7e2a474a495d213c7cb96e7bb6051a4c6c78ae
Dracony <draconyster@gmail.com>
2016-06-03 11:36:07 +0200

    social component

--- tests/PHPixie/Tests/Framework/ComponentsTest.php
+++ tests/PHPixie/Tests/Framework/ComponentsTest.php
@@ -8,37 +8,37 @@ namespace PHPixie\Tests\Framework;
 class ComponentsTest extends \PHPixie\Test\Testcase
 {
     protected $builder;
-        
+
     protected $components;
-    
+
     protected $configuration;
     protected $context;
     protected $extensions;
-    
+
     public function setUp()
     {
         $this->builder = $this->builder();
         $this->components = $this->components(null);
-        
+
         $this->configuration = $this->configuration();
         $this->method($this->builder, 'configuration', $this->configuration, array());
-        
+
         $this->context = $this->quickMock('\PHPixie\Framework\Context');
         $this->method($this->builder, 'context', $this->context, array());
-        
+
         $this->extensions = $this->extensions();
         $this->method($this->builder, 'extensions', $this->extensions, array());
     }
-    
+
     /**
      * @covers ::__construct
      * @covers ::<protected>
      */
     public function testConstruct()
     {
-        
+
     }
-    
+
     /**
      * @covers ::slice
      * @covers ::<protected>
@@ -47,7 +47,7 @@ class ComponentsTest extends \PHPixie\Test\Testcase
     {
         $this->assertComponent('slice', '\PHPixie\Slice');
     }
-    
+
     /**
      * @covers ::config
      * @covers ::<protected>
@@ -56,12 +56,12 @@ class ComponentsTest extends \PHPixie\Test\Testcase
     {
         $this->components = $this->components(array('slice'));
         $slice = $this->prepareComponent('slice');
-        
+
         $this->assertComponent('config', '\PHPixie\Config', array(
             'slice' => $slice
         ));
     }
-    
+
     /**
      * @covers ::debug
      * @covers ::<protected>
@@ -70,7 +70,7 @@ class ComponentsTest extends \PHPixie\Test\Testcase
     {
         $this->assertComponent('debug', '\PHPixie\Debug');
     }
-    
+
     /**
      * @covers ::database
      * @covers ::<protected>
@@ -78,15 +78,32 @@ class ComponentsTest extends \PHPixie\Test\Testcase
     public function testDatabase()
     {
         $configData = $this->prepareConfig('database');
-        
+
         $database = $this->components->database();
         $this->assertInstance($database, '\PHPixie\Database', array(
             'config' => $configData
         ));
-        
+
         $this->assertSame($database, $this->components->database());
     }
-    
+
+    /**
+     * @covers ::social
+     * @covers ::<protected>
+     */
+    public function testSocial()
+    {
+        $configData = $this->prepareConfig('social');
+
+        $social = $this->components->social();
+        $this->assertInstance($social, '\PHPixie\Social');
+        $this->assertInstance($social->builder(), '\PHPixie\Social\Builder', array(
+            'configData' => $configData
+        ));
+
+        $this->assertSame($social, $this->components->social());
+    }
+
     /**
      * @covers ::filesystem
      * @covers ::<protected>
@@ -95,7 +112,7 @@ class ComponentsTest extends \PHPixie\Test\Testcase
     {
         $this->assertComponent('filesystem', '\PHPixie\Filesystem');
     }
-    
+
     /**
      * @covers ::http
      * @covers ::<protected>
@@ -104,12 +121,12 @@ class ComponentsTest extends \PHPixie\Test\Testcase
     {
         $this->components = $this->components(array('slice'));
         $slice = $this->prepareComponent('slice');
-        
+
         $this->assertComponent('http', '\PHPixie\HTTP', array(
             'slice' => $slice
         ));
     }
-    
+
     /**
      * @covers ::httpProcessors
      * @covers ::<protected>
@@ -118,13 +135,13 @@ class ComponentsTest extends \PHPixie\Test\Testcase
     {
         $this->components = $this->components(array('http'));
         $http = $this->prepareComponent('http');
-        
+
         $this->assertComponent('httpProcessors', '\PHPixie\HTTPProcessors', array(
             'http' => $http
         ));
     }
-    
-    
+
+
     /**
      * @covers ::orm
      * @covers ::<protected>
@@ -134,17 +151,17 @@ class ComponentsTest extends \PHPixie\Test\Testcase
         $this->components = $this->components(array('database'));
         $database = $this->prepareComponent('database');
         $configData = $this->prepareConfig('orm');
-        
+
         $wrappers = $this->quickMock('\PHPixie\ORM\Wrappers');
         $this->method($this->configuration, 'ormWrappers', $wrappers, array());
-        
+
         $this->assertComponent('orm', '\PHPixie\ORM', array(
             'database'    => $database,
             'configSlice' => $configData,
             'wrappers'    => $wrappers,
         ));
     }
-    
+
     /**
      * @covers ::processors
      * @covers ::<protected>
@@ -153,7 +170,7 @@ class ComponentsTest extends \PHPixie\Test\Testcase
     {
         $this->assertComponent('processors', '\PHPixie\Processors');
     }
-    
+
     /**
      * @covers ::route
      * @covers ::<protected>
@@ -162,7 +179,7 @@ class ComponentsTest extends \PHPixie\Test\Testcase
     {
         $this->assertComponent('route', '\PHPixie\Route');
     }
-    
+
     /**
      * @covers ::template
      * @covers ::<protected>
@@ -171,21 +188,21 @@ class ComponentsTest extends \PHPixie\Test\Testcase
     {
         $this->components = $this->components(array('slice'));
         $slice = $this->prepareComponent('slice');
-        
+
         $configData = $this->prepareConfig('template');
         $locator    = $this->prepareLocator('template');
         $root       = $this->prepareRoot();
-        
+
         $templateExtensions = array(
             $this->quickMock('\PHPixie\Template\Extensions\Extension')
         );
         $this->method($this->extensions, 'templateExtensions', $templateExtensions, array(), 0);
-        
+
         $templateFormats = array(
             $this->quickMock('\PHPixie\Template\Formats\Format')
         );
         $this->method($this->extensions, 'templateFormats', $templateFormats, array(), 1);
-        
+
         $this->assertComponent('template', '\PHPixie\Template', array(
             'slice'              => $slice,
             'configData'         => $configData,
@@ -195,7 +212,7 @@ class ComponentsTest extends \PHPixie\Test\Testcase
             'externalFormats'    => $templateFormats
         ));
     }
-    
+
     /**
      * @covers ::auth
      * @covers ::<protected>
@@ -203,18 +220,18 @@ class ComponentsTest extends \PHPixie\Test\Testcase
     public function testAuth()
     {
         $this->components = $this->components(array('database'));
-        
+
         $configData = $this->prepareConfig('auth');
-        
+
         $repositories = $this->quickMock('\PHPixie\Auth\Repositories\Registry');
         $this->method($this->configuration, 'authRepositories', $repositories, array());
-        
+
         $this->assertComponent('auth', '\PHPixie\Auth', array(
             'configData' => $configData,
             'contextContainer' => $this->context
         ));
     }
-    
+
     /**
      * @covers ::authProcessors
      * @covers ::<protected>
@@ -223,12 +240,12 @@ class ComponentsTest extends \PHPixie\Test\Testcase
     {
         $this->components = $this->components(array('auth'));
         $auth = $this->prepareComponent('auth');
-        
+
         $this->assertComponent('authProcessors', '\PHPixie\AuthProcessors', array(
             'auth' => $auth
         ));
     }
-    
+
     /**
      * @covers ::paginate
      * @covers ::<protected>
@@ -237,7 +254,7 @@ class ComponentsTest extends \PHPixie\Test\Testcase
     {
         $this->assertComponent('paginate', '\PHPixie\Paginate');
     }
-    
+
     /**
      * @covers ::paginateOrm
      * @covers ::<protected>
@@ -246,70 +263,70 @@ class ComponentsTest extends \PHPixie\Test\Testcase
     {
         $this->components = $this->components(array('paginate'));
         $paginate = $this->prepareComponent('paginate');
-        
+
         $this->assertComponent('paginateOrm', '\PHPixie\PaginateORM', array(
             'paginate' => $paginate
         ));
     }
-    
+
     protected function assertComponent($name, $class, $builderAttributes = null)
     {
         $instance = $this->components->$name();
         $this->assertInstance($instance, $class);
-        
+
         if($builderAttributes !== null) {
             $builder = $instance->builder();
             foreach($builderAttributes as $key => $value) {
                 $this->assertAttributeEquals($value, $key, $builder);
             }
         }
-        
+
         $this->assertSame($instance, $this->components->$name());
     }
-    
+
     protected function prepareComponent($name)
     {
         $mock = $this->quickMock('\PHPixie\\'.ucfirst($name));
         $this->method($this->components, $name, $mock, array());
         return $mock;
     }
-    
+
     protected function prepareConfig($name)
     {
         $configData = $this->getSliceData();
         $this->method($this->configuration, $name.'Config', $configData, array());
         return $configData;
     }
-    
+
     protected function prepareLocator($name)
     {
         $locator = $this->getSliceData();
         $this->method($this->configuration, $name.'Locator', $locator, array());
         return $locator;
     }
-    
+
     protected function prepareRoot()
     {
         $root = $this->quickMock('\PHPixie\Filesystem\Root');
         $this->method($this->configuration, 'filesystemRoot', $root, array());
         return $root;
     }
-    
+
     protected function getSliceData()
     {
         return $this->quickMock('\PHPixie\Slice\Data');
     }
-    
+
     protected function extensions()
     {
         return $this->quickMock('\PHPixie\Framework\Extensions');
     }
-    
+
     protected function builder()
     {
         return $this->quickMock('\PHPixie\Framework\Builder');
     }
-    
+
     protected function components($methods)
     {
         return $this->getMock(
@@ -318,7 +335,7 @@ class ComponentsTest extends \PHPixie\Test\Testcase
             array($this->builder)
         );
     }
-    
+
     protected function configuration()
     {
         return $this->quickMock('\PHPixie\Framework\Configuration');
--- tests/PHPixie/Tests/Framework/ExtensionsTest.php
+++ tests/PHPixie/Tests/Framework/ExtensionsTest.php
@@ -11,28 +11,28 @@ class ExtenionsTest extends \PHPixie\Test\Testcase
     protected $http;
     protected $debug;
     protected $routeTranslator;
-    
+
     public function setUp()
     {
         $this->builder = $this->quickMock('\PHPixie\Framework\Builder');
         $this->extensions = $this->extensions();
-        
+
         $this->http = $this->quickMock('\PHPixie\Framework\HTTP');
         $this->method($this->builder, 'http', $this->http, array());
-        
+
         $this->routeTranslator = $this->quickMock('\PHPixie\Route\Translator');
         $this->method($this->http, 'routeTranslator', $this->routeTranslator, array());
     }
-    
+
     /**
      * @covers ::__construct
      * @covers ::<protected>
      */
     public function testConstruct()
     {
-        
+
     }
-    
+
     /**
      * @covers ::templateExtensions
      * @covers ::<protected>
@@ -50,7 +50,7 @@ class ExtenionsTest extends \PHPixie\Test\Testcase
         $this->assertTemplateDebugExtension($extensions[0], $debug);
         $this->assertTemplateRouteExtension($extensions[1]);
     }
-    
+
     /**
      * @covers ::templateFormats
      * @covers ::<protected>
@@ -59,7 +59,7 @@ class ExtenionsTest extends \PHPixie\Test\Testcase
     {
         $this->assertSame(array(), $this->extensions->templateFormats());
     }
-    
+
     /**
      * @covers ::authProviderBuilders
      * @covers ::<protected>
@@ -68,30 +68,38 @@ class ExtenionsTest extends \PHPixie\Test\Testcase
     {
         $extensions = $this->extensionsMock(array(
             'buildAuthLogin',
-            'buildAuthHttp'
+            'buildAuthHttp',
+            'buildAuthSocial'
         ));
-        
+
         $authLogin = $this->quickMock('\PHPixie\AuthLogin');
         $this->method($extensions, 'buildAuthLogin', $authLogin, array(), 0);
-        
+
         $authLoginProviders = $this->quickMock('\PHPixie\AuthLogin\Providers');
         $this->method($authLogin, 'providers', $authLoginProviders, array(), 0);
-        
+
         $authHttp  = $this->quickMock('\PHPixie\AuthHTTP');
         $this->method($extensions, 'buildAuthHttp', $authHttp, array(), 1);
-        
+
         $authHttpProviders = $this->quickMock('\PHPixie\AuthHTTP\Providers');
         $this->method($authHttp, 'providers', $authHttpProviders, array(), 0);
-        
+
+        $authSocial  = $this->quickMock('\PHPixie\AuthSocial');
+        $this->method($extensions, 'buildAuthSocial', $authSocial, array(), 2);
+
+        $authSocialProviders = $this->quickMock('\PHPixie\AuthSocial\Providers');
+        $this->method($authSocial, 'providers', $authSocialProviders, array(), 0);
+
         $this->assertSame(
             array(
                 $authLoginProviders,
-                $authHttpProviders
+                $authHttpProviders,
+                $authSocialProviders
             ),
             $extensions->authProviderBuilders()
         );
     }
-    
+
     /**
      * @covers ::buildAuthLogin
      * @covers ::<protected>
@@ -100,16 +108,17 @@ class ExtenionsTest extends \PHPixie\Test\Testcase
     {
         $components = $this->getComponents();
         $this->method($this->builder, 'components', $components, array(), 0);
-        
+
         $security = $this->quickMock('\PHPixie\Security');
         $this->method($components, 'security', $security, array(), 0);
-        
+
         $authLogin = $this->extensions->buildAuthLogin();
         $this->assertInstance($authLogin, '\PHPixie\AuthLogin');
         $this->assertInstance($authLogin->builder(), '\PHPixie\AuthLogin\Builder', array(
             'security'             => $security
         ));
     }
+
     /**
      * @covers ::buildAuthHttp
      * @covers ::<protected>
@@ -118,18 +127,36 @@ class ExtenionsTest extends \PHPixie\Test\Testcase
     {
         $components = $this->getComponents();
         $this->method($this->builder, 'components', $components, array(), 0);
-        
+
         $security = $this->quickMock('\PHPixie\Security');
         $this->method($components, 'security', $security, array(), 0);
-        
+
         $context = $this->quickMock('\PHPixie\Framework\Context');
         $this->method($this->builder, 'context', $context, array(), 1);
-        
+
         $authHttp = $this->extensions->buildAuthHttp();
         $this->assertInstance($authHttp, '\PHPixie\AuthHttp');
         $this->assertInstance($authHttp->builder(), '\PHPixie\AuthHttp\Builder', array(
             'security'             => $security,
             'httpContextContainer' => $context
+        ));
+    }
+
+    /**
+     * @covers ::buildAuthSocial
+     * @covers ::<protected>
+     */
+    public function testBuildAuthSocial()
+    {
+        $components = $this->getComponents();
+        $this->method($this->builder, 'components', $components, array(), 0);
+
+        $social = $this->quickMock('\PHPixie\Social');
+        $this->method($components, 'social', $social, array(), 0);
+
+        $authSocial = $this->extensions->buildAuthSocial();
+        $this->assertInstance($authSocial, '\PHPixie\AuthSocial', array(
+            'social' => $social
         ));
     }
 
@@ -149,17 +176,17 @@ class ExtenionsTest extends \PHPixie\Test\Testcase
             'routeTranslator' => $this->routeTranslator
         ));
     }
-    
+
     protected function getComponents()
     {
         return $this->quickMock('\PHPixie\Framework\Components');
     }
-    
+
     protected function extensions()
     {
         return new \PHPixie\Framework\Extensions($this->builder);
     }
-    
+
     protected function extensionsMock($methods)
     {
         return $this->quickMock(
